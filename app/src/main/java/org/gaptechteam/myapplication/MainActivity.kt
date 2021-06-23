71f2cd9057ba8077340c3531417f5bee5933ca26
Yolando Asri Erbenca Gegeh <yolandomanullang@student.telkomuniversity.ac.id>
2021-06-23 23:21:41 +0700

    Done Nav Fragment Bottom

--- app/src/main/java/org/gaptechteam/myapplication/MainActivity.kt
+++ app/src/main/java/org/gaptechteam/myapplication/MainActivity.kt
@@ -2,8 +2,12 @@ package org.gaptechteam.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.gaptechteam.myapplication.fragment.AkunFragment
 import org.gaptechteam.myapplication.fragment.DarahFragment
 import org.gaptechteam.myapplication.fragment.HomeFragment
@@ -16,12 +20,51 @@ class MainActivity : AppCompatActivity() {
     val fm : FragmentManager =supportFragmentManager
     var active : Fragment = fragmentHome
 
+    private lateinit var menu : Menu
+    private lateinit var menuItem: MenuItem
+    private lateinit var bottonNavigationView: BottomNavigationView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setUpBottoNav()
+
+    }
+
+    fun setUpBottoNav(){
         fm.beginTransaction().add(R.id.container,fragmentHome).show(fragmentHome).commit()
         fm.beginTransaction().add(R.id.container,fragmentAkun).hide(fragmentAkun).commit()
         fm.beginTransaction().add(R.id.container,fragmentDarah).hide(fragmentDarah).commit()
+
+        bottonNavigationView = findViewById(R.id.nav_view)
+        menu = bottonNavigationView.menu
+        menuItem = menu.getItem(0)
+        menuItem.isChecked = true
+
+        bottonNavigationView.setOnNavigationItemReselectedListener { item ->
+
+            when(item.itemId) {
+                R.id.navigation_home ->{
+                    callFragment(0,fragmentHome)
+                }
+                R.id.navigation_darah ->{
+                    callFragment(1,fragmentDarah)
+                }
+                R.id.navigation_akun ->{
+                    callFragment(2,fragmentAkun)
+
+                }
+
+            }
+            false
+        }
+    }
+    fun callFragment(int : Int,fragment: Fragment){
+        menuItem = menu.getItem(int)
+        menuItem.isChecked = true
+        fm.beginTransaction().hide(active).show(fragment).commit()
+        active = fragment
     }
 }
