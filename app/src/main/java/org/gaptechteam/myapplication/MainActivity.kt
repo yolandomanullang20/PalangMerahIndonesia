80af6f017722e16ee9a99c2d5868d81553424543
Yolando Asri Erbenca Gegeh <yolandomanullang@student.telkomuniversity.ac.id>
2021-06-23 22:53:06 +0700

    create layout buttom nav

--- app/src/main/java/org/gaptechteam/myapplication/MainActivity.kt
+++ app/src/main/java/org/gaptechteam/myapplication/MainActivity.kt
@@ -2,10 +2,26 @@ package org.gaptechteam.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import org.gaptechteam.myapplication.fragment.AkunFragment
+import org.gaptechteam.myapplication.fragment.DarahFragment
+import org.gaptechteam.myapplication.fragment.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+
+    val fragmentHome : Fragment = HomeFragment()
+    val fragmentDarah : Fragment = DarahFragment()
+    val fragmentAkun : Fragment = AkunFragment()
+    val fm : FragmentManager =supportFragmentManager
+    var active : Fragment = fragmentHome
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fm.beginTransaction().add(R.id.container,fragmentHome).show(fragmentHome).commit()
+        fm.beginTransaction().add(R.id.container,fragmentAkun).hide(fragmentAkun).commit()
+        fm.beginTransaction().add(R.id.container,fragmentDarah).hide(fragmentDarah).commit()
     }
 }
