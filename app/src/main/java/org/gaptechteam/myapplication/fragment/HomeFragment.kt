2c985b89c9d1c2779890e1b722abb414862e140b
Yolando Asri Erbenca Gegeh <yolandomanullang@student.telkomuniversity.ac.id>
2021-06-24 00:18:59 +0700

    done slider

--- app/src/main/java/org/gaptechteam/myapplication/fragment/HomeFragment.kt
+++ app/src/main/java/org/gaptechteam/myapplication/fragment/HomeFragment.kt
@@ -5,15 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager.widget.ViewPager
 import org.gaptechteam.myapplication.R
+import org.gaptechteam.myapplication.adapter.AdapterSlider
 
 class HomeFragment : Fragment() {
+
+    lateinit var vpSlider : ViewPager
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+        val view : View = inflater.inflate(R.layout.fragment_home, container, false)
+        vpSlider = view.findViewById(R.id.vp_slider)
+
+        val arraySlider = ArrayList<Int>()
+        arraySlider.add(R.drawable.slider1)
+        arraySlider.add(R.drawable.slider2)
+        arraySlider.add(R.drawable.slider3)
+
+        val adapterSlide = AdapterSlider(arraySlider,activity)
+        vpSlider.adapter = adapterSlide
+        return view
     }
 
 
